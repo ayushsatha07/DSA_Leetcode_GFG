740f61e10612c6ae1536f7e060480fec27b97988
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-06-26 22:02:14 +0530

    Binary Search Solution

---
+++ src/BinarySearch/binarySeach.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+// Time Complexity :- O(logn)
+// Space Complexity :-O(1)
+
+class Solution {
+    public int search(int[] arr, int target) {
+        int low=0 ,high=arr.length-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>target){
+                high=mid-1;
+            }else if(target>arr[mid]){
+                low=mid+1;
+            }else{
+                return mid;
+
+            }
+        }
+        return -1;
+    }
+}
