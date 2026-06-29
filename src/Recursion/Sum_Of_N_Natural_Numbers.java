32e60f3f5293bcea48fc73514663c48f1e115c5b
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-06-29 11:02:35 +0530

    Sum of N natural Number GFG solution

---
+++ src/Recursion/Sum_Of_N_Natural_Numbers.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Sum_Of_N_Natural_Numbers {
+    static void main(String[] args) {
+        int n=100;
+        System.out.println("Sum of "+n+" Natural Number : "+sum(n));
+    }
+    public static int sum(int n){
+        if(n==0) return 0;
+        return n+sum(n-1);
+    }
+}
