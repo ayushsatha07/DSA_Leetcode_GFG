bbc47eadad999ea32de3674d758bf60d7a8679a2
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-06-30 14:40:17 +0530

    Unique Paths Leetcode Solution

---
+++ src/Recursion/Unique_paths_Leetcode62.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+//Time Complexity :-O(2^(m+n))
+//Space Complexity:-O(m+n)
+
+public class Unique_paths_Leetcode62 {
+    static void main(String[] args) {
+        int m=5 , n=6;
+        System.out.println("Uniuqe Paths : "+uniquePaths(m,n));
+    }
+    public static int uniquePaths(int m, int n){
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+        return uniquePaths(m,n-1)+uniquePaths(m-1,n);
+    }
+}
