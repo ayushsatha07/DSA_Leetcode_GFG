adf7039a41e9d318eac9be8d727ed002bb60ac7d
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-06-30 14:30:21 +0530

    GCD Of Two Number Solution GFG Practice

---
+++ src/Recursion/GCD_Of_Two_Numbers_GFG.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+//Time Complexity :- O(log(min(a,b))
+//Space Complexity :-O(log(min(a,b))
+
+public class GCD_Of_Two_Numbers_GFG {
+    static void main(String[] args) {
+        int a=12, b=16;
+        System.out.println("GCD of Two Numbers : "+gcd(a,b));
+    }
+    public static int gcd(int a, int b){
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+}
