61f5002771d7b2f174e6291ca54f751fadeedd5a
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-01 20:00:08 +0530

    Palindrome Solution

---
+++ src/JavaStrings/Palindrome.java
@@ -0,0 +1,18 @@
+package JavaStrings;
+
+public class Palindrome {
+    static void main(String[] args) {
+        String s ="racecar";
+        String s1="rahul";
+        System.out.println(s+" is Palindrome : "+isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s){
+        int i=0 , j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
