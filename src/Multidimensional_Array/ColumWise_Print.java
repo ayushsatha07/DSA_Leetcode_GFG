01189543c4e2e66ed0ebbf7902658b71e2a86b92
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-02 17:08:49 +0530

    ColumnWise Printing

---
+++ src/Multidimensional_Array/ColumWise_Print.java
@@ -0,0 +1,16 @@
+package Multidimensional_Array;
+
+public class ColumWise_Print {
+    static void main(String[] args) {
+        int arr[][]={{22,33,44,52},{33,56,43,21},{32,78,549,32},{32,67,43,89}};
+        printColumnWise(arr);
+    }
+    public static void printColumnWise(int arr[][]){
+        for(int i=0 ; i<arr[0].length ; i++){
+            for(int j=0 ; j<arr.length ; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
