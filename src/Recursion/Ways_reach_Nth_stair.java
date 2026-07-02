82a872e678c156c3276f619e43fa03735994e132
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-02 13:04:36 +0530

    Number of Ways to reach Nth Stair

---
+++ src/Recursion/Ways_reach_Nth_stair.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Ways_reach_Nth_stair {
+    static void main(String[] args) {
+        int ways=countWays(5);
+        System.out.println("Number of Ways : "+ways);
+    }
+        public static int countWays(int n) {
+            // code here
+            if(n<=2) return n;
+            return countWays(n-1)+countWays(n-2);
+        }
+}
