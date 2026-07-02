492230a568f3a7b5a34f5d6b699ed141d9e01e6a
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-02 12:54:05 +0530

    Flor in Sorted Array Solution

---
+++ src/BinarySearch/Flor_In_Sorrted_Array.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class Flor_In_Sorrted_Array {
+    static void main(String[] args) {
+        int arr[]={1,22,54,76,89,90,93};
+        int tar=56;
+        int idx=flor(arr ,tar);
+        System.out.println("Index of flor : "+flor(arr,tar));
+    }
+    public static int flor(int arr[] , int tar){
+        int low=0 , high=arr.length-1;
+        int idx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>tar) high=mid-1;
+            else if (arr[mid]<=tar) {
+                idx=mid;
+                low=mid+1;
+            }
+        }
+        return idx;
+    }
+}
