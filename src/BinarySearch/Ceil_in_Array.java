f323708dcd4db0ab22c745c1ccd4e750d105e899
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-03 12:48:00 +0530

    Ceil in Sorted Array

---
+++ src/BinarySearch/Ceil_in_Array.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class Ceil_in_Array {
+    static void main(String[] args) {
+        int arr[]={11,43,56,75,90,96};
+        System.out.println("Ceil In Array :"+ceil(arr,70));
+    }
+    public static int ceil(int arr[] , int tar){
+        int low=0 , high=arr.length-1;
+        int idx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<tar) low=mid+1;
+            else if(arr[mid]>=tar){
+                idx=mid;
+                high=mid-1;
+            }
+        }
+        return idx;
+    }
+}
