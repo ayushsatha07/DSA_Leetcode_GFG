558f133ddb34708c7e7003c446253d16f482a587
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-05 10:14:12 +0530

    Score of string

---
+++ src/JavaStrings/ScoreOfString.java
@@ -0,0 +1,18 @@
+package JavaStrings;
+
+public class ScoreOfString {
+    static void main(String[] args) {
+        String s="hello";
+        System.out.println("Score :"+score(s));
+    }
+    public static int score(String s){
+        int sum=0;
+        int j=0;
+        for(int i=1 ;i<s.length() ; i++){
+            int diff=s.charAt(i)-s.charAt(j);
+            if(diff<0) diff=diff*-1;
+            sum=sum+diff;
+        }
+        return sum;
+    }
+}
