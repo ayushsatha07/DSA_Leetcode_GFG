5d09e113c0571f991da54350bf6c1425b1cd6f1f
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-10 22:27:32 +0530

    Check Array Is sorted

---
+++ src/Arrays/Check_ArraySorted.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Check_ArraySorted {
+    static void main(String[] args) {
+        int arr[]={44,23,56,78,54,22};
+        int brr[]={11,22,33,44,55,66};
+        int crr[]={12,32,43,89,95,91};
+        System.out.println(isSorted(crr));
+    }
+    public static boolean isSorted(int arr[]){
+        int n=arr.length;
+        for(int i=1 ; i<n ; i++){
+            if(arr[i-1]>arr[i]) return false;
+        }
+        return true;
+    }
+}
