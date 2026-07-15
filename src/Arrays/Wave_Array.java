e90c10c71364a940ba9c5f2b7f3b2c475ca6a8c5
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-07-15 22:05:46 +0530

    Wave Array Solution

---
+++ src/Arrays/Wave_Array.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+
+public class Wave_Array {
+
+    class Solution {
+        public void sortInWave(int arr[]) {
+        int i=0,j=1;
+        while(i<arr.length && j<arr.length){
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i=i+2;
+            j=j+2;
+            }
+
+        }
+    }
+}
