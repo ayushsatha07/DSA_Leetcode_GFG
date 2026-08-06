37b8ee26dc856386907c8fdb4467dbbec4aa1191
Ayush Sathawane <ayushsathawane406@gmail.com>
2026-08-06 23:30:05 +0530

    updating time Complexity
    Fix formatting issues in GCD calculation code.

--- src/Recursion/GCD_Of_Two_Numbers_GFG.java
+++ src/Recursion/GCD_Of_Two_Numbers_GFG.java
@@ -2,8 +2,6 @@ package Recursion;
 
 //Time Complexity :- O(log(min(a,b))
 //Space Complexity :-O(log(min(a,b))
-
-
 public class GCD_Of_Two_Numbers_GFG {
     static void main(String[] args) {
         int a=12, b=16;
